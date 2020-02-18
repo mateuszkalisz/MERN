437669495bfab779a942f06a35f3504c8f0d3a14
MK <kaliszmat1@gmail.com>
2020-02-18 23:45:06 +0100

    styling user page

--- APP/frontend/src/user/components/UserItem.js
+++ APP/frontend/src/user/components/UserItem.js
@@ -1,12 +1,17 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 import "./UserItem.css";
+
 
 const UserItem = props => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
+      <Card className="user-item__content">
+          <Link to={`/${props.id}/places`}>
         <div className="user-item__image">
-          <img src={props.image} alt={props.name} />
+          <Avatar name={props.name} image={props.image} />
         </div>
         <div className="user-item__info">
           <h2>{props.name}</h2>
@@ -14,7 +19,8 @@ const UserItem = props => {
             {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
           </h3>
         </div>
-      </div>
+          </Link>
+      </Card>
     </li>
   );
 };
