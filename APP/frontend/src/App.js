ccad4c029450f9710caa58057788cdb1938b1388
MK <kaliszmat1@gmail.com>
2020-02-19 20:51:45 +0100

    create a navigation (using ReactDOM.createPortal, react router, react-transition-group) based on lesson

--- APP/frontend/src/App.js
+++ APP/frontend/src/App.js
@@ -8,10 +8,14 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
+
   return (
     <Router>
+      <MainNavigation/>
+      <main>
       <Switch>
         <Route path="/places/new" exact>
           <NewPlace />
@@ -21,6 +25,7 @@ const App = () => {
         </Route>
         <Redirect to="/" />
       </Switch>
+      </main>
     </Router>
   );
 };
