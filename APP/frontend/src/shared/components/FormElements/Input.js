f4ce9b8f1a63309efb0f19ff0a83aa7f43739dbf
MK <kaliszmat1@gmail.com>
2020-02-25 20:53:06 +0100

    create ability to add new place (without backend - only schema)

--- APP/frontend/src/shared/components/FormElements/Input.js
+++ APP/frontend/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import { validate } from "../../util/validators";
 
 import "./Input.css";
@@ -28,6 +28,13 @@ const Input = props => {
     isTouch: false
   });
 
+  const {id, onInput} = props;
+  const {value, isValid} = inputState;
+
+ useEffect(() => {
+    onInput(id, value, isValid)
+  }, [id, value, isValid, onInput]);
+  
   const changeHandler = event => {
     dispatch({
       type: "CHANGE",
@@ -49,6 +56,7 @@ const Input = props => {
         onChange={changeHandler}
         onBlur={touchHandler}
         value={inputState.value}
+        
       />
     ) : (
       <textarea
