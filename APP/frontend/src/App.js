50b7901a5f5652bfc3e1e467a21c670f50277664
MK <kaliszmat1@gmail.com>
2020-02-29 17:11:14 +0100

    fix issues with updateplace

--- APP/frontend/src/App.js
+++ APP/frontend/src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
   Switch
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import Users from "./user/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import UserPlaces from "./places/pages/UserPlaces";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UpdatePlace from "./places/pages/UpdatePlace";
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
@@ -27,7 +27,7 @@ const App = () => {
           <Route path="/places/new" exact>
             <NewPlace />
           </Route>
-          <Route path="/places/:placeId" exact>
+          <Route path="/places/:placeId">
             <UpdatePlace />
           </Route>
           <Redirect to="/" />
--- APP/frontend/src/shared/components/FormElements/Input.js
+++ APP/frontend/src/shared/components/FormElements/Input.js
@@ -1,21 +1,22 @@
-import React, { useReducer, useEffect } from "react";
-import { validate } from "../../util/validators";
+import React, { useReducer, useEffect } from 'react';
 
-import "./Input.css";
+import { validate } from '../../util/validators';
+import './Input.css';
 
 const inputReducer = (state, action) => {
   switch (action.type) {
-    case "CHANGE":
+    case 'CHANGE':
       return {
         ...state,
         value: action.val,
         isValid: validate(action.val, action.validators)
       };
-    case "TOUCH":
+    case 'TOUCH': {
       return {
         ...state,
-        isTouch: true
-      };
+        isTouched: true
+      }
+    }
     default:
       return state;
   }
@@ -23,32 +24,34 @@ const inputReducer = (state, action) => {
 
 const Input = props => {
   const [inputState, dispatch] = useReducer(inputReducer, {
-    value: props.value || "",
-    isValid: props.valid || false,
-    isTouch: false
+    value: props.initialValue || '',
+    isTouched: false,
+    isValid: props.initialValid || false
   });
 
-  const {id, onInput} = props;
-  const {value, isValid} = inputState;
+  const { id, onInput } = props;
+  const { value, isValid } = inputState;
 
- useEffect(() => {
+  useEffect(() => {
     onInput(id, value, isValid)
   }, [id, value, isValid, onInput]);
-  
+
   const changeHandler = event => {
     dispatch({
-      type: "CHANGE",
+      type: 'CHANGE',
       val: event.target.value,
       validators: props.validators
     });
   };
 
   const touchHandler = () => {
-    dispatch({ type: "TOUCH" });
+    dispatch({
+      type: 'TOUCH'
+    });
   };
 
   const element =
-    props.element === "input" ? (
+    props.element === 'input' ? (
       <input
         id={props.id}
         type={props.type}
@@ -56,7 +59,6 @@ const Input = props => {
         onChange={changeHandler}
         onBlur={touchHandler}
         value={inputState.value}
-        
       />
     ) : (
       <textarea
@@ -70,12 +72,12 @@ const Input = props => {
 
   return (
     <div
-      className={`form-control ${!inputState.isValid && inputState.isTouch &&
-        "form-control--invalid"}`}
+      className={`form-control ${!inputState.isValid && inputState.isTouched &&
+        'form-control--invalid'}`}
     >
       <label htmlFor={props.id}>{props.label}</label>
       {element}
-      {!inputState.isValid && inputState.isTouch && <p>{props.errorText}</p>}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
     </div>
   );
 };
