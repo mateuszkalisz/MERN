a5df5b2cc48365e78b6b3f65e5d51ecd72aa24b0
MK <kaliszmat1@gmail.com>
2020-04-18 19:12:24 +0200

    add input handler for new note form

--- PROJECT/frontend/src/notes/pages/NewNote.js
+++ PROJECT/frontend/src/notes/pages/NewNote.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./NewNote.css";
 
 const NewNote = () => {
 
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [category, setCategory] = useState("SQL");
+  const [privacy, setPrivacy] = useState(false);
+
+  const titleHandler = (e) =>{
+    setTitle(e.target.value);
+  }
+
+  const contentHandler = (e) =>{
+    setContent(e.target.value);
+  }
+
+  const categoryHandler = (e) =>{
+    setCategory(e.target.value);
+  }
+
+  const privacyHandler = () =>{
+    setPrivacy(prevState => !prevState);
+  }
+
   const newNoteHandler = (e) =>{
     e.preventDefault();
-    console.log("new note has been created");
+    console.log("new note has been created", [title, content,category,privacy]);
   }
 
   return (
     <div className="newNote">
-      <form classname="newNoteForm" onSubmit={newNoteHandler}>
+      <form className="newNoteForm" onSubmit={newNoteHandler}>
         <div className="newTitle newInput newFormElement">
           <label htmlFor="newTitle">Title</label>
-          <input type="text" id="newTitle" />
+          <input type="text" id="newTitle" value={title} onChange={titleHandler}/>
         </div>
         <div className="newContent newInput newFormElement">
           <label htmlFor="newContent">Content</label>
-          <textarea id="newContent" />
+          <textarea id="newContent" value={content} onChange={contentHandler}/>
         </div>
         <div className="selectCategory newInput newFormElement">
           <label htmlFor="selectCategory">Category</label>
-          <select name="selectCategory" id="selectCategory">
+          <select name="selectCategory" id="selectCategory" value={category} onChange={categoryHandler}>
             <option value="SQL">SQL/Database</option>
             <option value="ETL">ETL/Komodo</option>
             <option value="SCRIPT">Scripting</option>
@@ -33,7 +54,7 @@ const NewNote = () => {
         </div>
         <div className="newPrivacy newFormElement">
           <label htmlFor="newPrivacy">Private</label>
-          <input type="checkbox" id="newPrivacy" />
+          <input type="checkbox" id="newPrivacy" checked={privacy} onChange={privacyHandler}/>
           <h4 className="newPolicy" style={{ display: "none" }}>
             The note will be visible only on your account.
           </h4>
