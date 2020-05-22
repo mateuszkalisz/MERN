68c09c672c5fdff9760b3758afbc6e6c6ba9a756
MK <kaliszmat1@gmail.com>
2020-05-22 22:56:13 +0200

    add other note routes to mongoose

--- PROJECT/backend/controllers/notes-controllers.js
+++ PROJECT/backend/controllers/notes-controllers.js
@@ -4,14 +4,25 @@ const HttpError = require('../models/http-error');
 const {validationResult} = require('express-validator');
 const Note = require('../models/notes');
 
-exports.getAllNotes = (req,res,next) =>{
-    const notes = DUMMY_NOTES;
+exports.getAllNotes = async (req,res,next) =>{
+    // const notes = DUMMY_NOTES;
+
+    let notes;
+
+    try{
+        notes = await Note.find({});
+    }
+    catch(err){
+        const error = new HttpError(err, 500);
+        return next(error);
+    }
+    
 
     if(notes.length === 0){
       next(new HttpError('Could not find a notes for this user id', 404));
     }
 
-    res.json({notes});
+    res.json({notes: notes.map(note => note.toObject({getters: true}))});
 };
 
 exports.getSelectedNote = async (req,res,next)=>{
@@ -102,7 +113,7 @@ exports.createNewNote = async (req,res,next) =>{
     res.status(201).json({createdNote});
 };
 
-exports.updateNote = (req,res,next)=>{
+exports.updateNote = async (req,res,next)=>{
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
@@ -112,46 +123,96 @@ exports.updateNote = (req,res,next)=>{
     const noteId = req.params.noteId;
     const {title, description, privacy} = req.body;
 
-    const updatedNote = DUMMY_NOTES.find(note => note.id.toString() === noteId.toString());
+    // const updatedNote = DUMMY_NOTES.find(note => note.id.toString() === noteId.toString());
+
+    let updatedNote;
+
+    try{
+        updatedNote = await Note.findById(noteId);
+    }
+    catch(err){
+        const error = new HttpError(err, 500);
+        return next(error);
+    }
+
 
     if(!updatedNote){
         const error = new HttpError('Could not find a note for this note id', 404)
         next(error);
     };
 
-    const noteIndex = DUMMY_NOTES.findIndex(note => note.id.toString() === noteId);
+    // const noteIndex = DUMMY_NOTES.findIndex(note => note.id.toString() === noteId);
 
     updatedNote.title = title;
     updatedNote.description = description;
     updatedNote.privacy = privacy;
     updatedNote.updatedDate = new Date().toLocaleDateString();
 
+    try{
+        await updatedNote.save();
+    }
+    catch(err){
+        const error = new HttpError(err, 500);
+        return next(error);
+    }
 
-    DUMMY_NOTES[noteIndex] = updatedNote;
+    // DUMMY_NOTES[noteIndex] = updatedNote;
 
-    res.status(200).json({updatedNote});
+    res.status(200).json({updatedNote: updatedNote.toObject({getters: true})});
 };
 
-exports.deleteNote = (req,res,next) =>{
+exports.deleteNote = async (req,res,next) =>{
     const noteId = req.params.noteId;
-    const deletedNote = DUMMY_NOTES.find(note => note.id.toString() === noteId.toString());
+
+    let deletedNote;
+
+    try{
+        deletedNote = await Note.findById(noteId);
+    }
+    catch(err){
+        const error = new HttpError(err, 500);
+        return next(error);
+    }
+
+    // const deletedNote = DUMMY_NOTES.find(note => note.id.toString() === noteId.toString());
 
     if(!deletedNote){
         const error = new HttpError('Could not find a note for this note id', 404)
         next(error);
     };
 
-    const noteIndex = DUMMY_NOTES.findIndex(note => note.id.toString() === noteId);
+    // const noteIndex = DUMMY_NOTES.findIndex(note => note.id.toString() === noteId);
 
-    DUMMY_NOTES.splice(noteIndex, 1);
+    // DUMMY_NOTES.splice(noteIndex, 1);
+
+    try{
+        await deletedNote.remove();
+    }
+
+    catch(err){
+        const error = new HttpError(err, 500);
+        return next(error);
+    }
 
     res.status(200).json(`${deletedNote.title} has been successfully deleted`);
 };
 
-exports.findNoteByContent = (req,res,next) =>{
-    const {content} = req.query;
+exports.findNoteByContent = async (req,res,next) =>{
+    const {description} = req.query;
 
-    const selectedNotes = DUMMY_NOTES.filter(note => note.description.toLowerCase().includes(content.toLowerCase()));
+    // const selectedNotes = DUMMY_NOTES.filter(note => note.description.toLowerCase().includes(content.toLowerCase()));
 
-    res.json({selectedNotes});
+    let selectedNotes;
+
+    try{
+        selectedNotes = await Note.find({
+            "description": { "$regex": description, "$options": "i" }
+        });    
+    }
+    catch(err){
+        const error = new HttpError(err, 500);
+        return next(error);
+    }
+    
+    res.json({selectedNotes: selectedNotes.map(note => note.toObject({getters: true}))});
 }
