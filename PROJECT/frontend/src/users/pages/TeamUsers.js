c2dc433437d433958e7554a90b994cdd4efa96b2
MK <kaliszmat1@gmail.com>
2020-06-04 23:03:54 +0200

    create http request (fetch) to get team users

--- PROJECT/frontend/src/users/pages/TeamUsers.js
+++ PROJECT/frontend/src/users/pages/TeamUsers.js
@@ -1,69 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import UserList from "../components/UserList";
 import { AuthContext } from "../../shared/context/AuthContext";
+import Modal from "../../shared/UIElements/Modal";
+import LoadingSpinner from "../../shared/UIElements/LoadingSpinner";
 
 import "./TeamUsers.css";
 
 const TeamUsers = () => {
   const auth = useContext(AuthContext);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(null);
+  const [team, setTeam] = useState([]);
 
-  const DUMMY_USER = [
-    {
-      id: 1,
-      name: "Mateusz",
-      password: "haslo123",
-      email: "mateusz@mk.pl",
-      team: "CoreFR",
-      notes: [
-        { id: 1, place: "pierwsze miejsce" },
-        { id: 2, place: "drugie miejsce" },
-      ],
-    },
-    {
-      id: 2,
-      name: "Maja",
-      password: "haslo123",
-      email: "maja@ms.pl",
-      team: "CoreFR",
-      notes: [
-        { id: 1, place: "pierwsze miejsce" },
-        { id: 2, place: "drugie miejsce" },
-        { id: 3, place: "trzecie miejsce" },
-      ],
-    },
-    {
-      id: 3,
-      name: "Jurek",
-      password: "haslo123",
-      email: "jurek@jb.pl",
-      team: "CoreFR",
-      notes: [
-        { id: 1, place: "pierwsze miejsce" },
-        { id: 2, place: "drugie miejsce" },
-        { id: 3, place: "trzecie miejsce" },
-      ],
-    },
-    {
-      id: 4,
-      name: "Andrzej",
-      password: "haslo123",
-      email: "andrzej@az.pl",
-      team: "WOBE",
-      notes: [
-        { id: 1, place: "pierwsze miejsce" },
-        { id: 2, place: "drugie miejsce" },
-      ],
-    },
-  ];
+  const closeModal = () =>{
+    setIsInvalid(null);
+  }
 
-  const teamUsers = DUMMY_USER.filter(
-    (user) => user.team === auth.teamId && user.id !== auth.userId
-  );
+  useEffect(()=>{
+    const getTeamUsersHandler = async () =>{
+      try{
+        setIsLoading(true);
+        const response = await fetch(`http://localhost:5000/team/${auth.teamId}`);
+
+        const jsonData = await response.json();
+
+        setIsLoading(false);
+
+        if(!response.ok){
+          setError(jsonData.message);
+          setIsInvalid(true);
+
+        }
+        setTeam(jsonData.teamUsers);
+      }
+      catch(err){
+        setIsLoading(false);
+        setError(err.message);
+        setIsInvalid(true);
+      }
+    }
+    getTeamUsersHandler();
+  }, [])
+
 
   return (
-    <div className="teamUsers">
-      <UserList users={teamUsers} />
-    </div>
+    <>
+      {isInvalid ? <Modal onClose={closeModal} className='error' header='Error' content={error}/> : null}
+      {isLoading ? <LoadingSpinner/> : null}
+      <div className="teamUsers">
+        <UserList users={team} />
+      </div>
+    </>
   );
 };
 
